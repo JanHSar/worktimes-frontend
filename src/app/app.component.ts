5a799111fb83fe5ac09e866e5618eb48f0665c8d
Jan Sarstedt <sarstedt.jan@gmail.com>
2024-01-03 11:15:56 +0100

    Add api service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './service/api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [HttpClientModule,CommonModule, RouterOutlet],
+  providers: [ApiService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/service/api.service.ts
@@ -0,0 +1,80 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  
+  // Connect to API on this url
+  private apiUrl: string = 'http://localhost:3000';
+  
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  /**
+   * GET request
+   * @param path 
+   * @param options 
+   * @returns API response
+   */
+  async get(path: string, options: any = null): Promise<any> {
+    return await firstValueFrom(
+      this.http.get(this.apiUrl + path, options)
+      );
+  }
+
+  /**
+   * POST request
+   * @param path 
+   * @param data 
+   * @param options 
+   * @returns API response
+   */
+  async post(path: string, data: object, options: any = {}): Promise<any> {
+    return await firstValueFrom(
+      this.http.post(this.apiUrl + path, data, options)
+    );
+  }
+
+    /**
+   * PUT request
+   * @param path 
+   * @param data 
+   * @param options 
+   * @returns API response
+   */
+    async put(path: string, data: object, options: any = null): Promise<any> {
+      return await firstValueFrom(
+        this.http.put(this.apiUrl + path, data, options)
+      );
+    }
+
+  /**
+   * PATCH request
+   * @param path 
+   * @param data 
+   * @param options 
+   * @returns API response
+   */
+  async patch(path: string, data: object, options: any = null): Promise<any> {
+    return await firstValueFrom(
+      this.http.patch(this.apiUrl + path, data, options)
+    );
+  }
+
+    /**
+   * DELETE request
+   * @param path 
+   * @param data 
+   * @param options 
+   * @returns API response
+   */
+    async delete(path: string, options: any = null): Promise<any> {
+      return await firstValueFrom(
+        this.http.delete(this.apiUrl + path, options)
+      );
+    }
+}
